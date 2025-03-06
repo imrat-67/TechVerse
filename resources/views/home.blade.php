c4bd4b7799ba1359bfc5a6cffb89d38bac83d78b
imrat-67 <imtiajhossain7862@gmail.com>
2025-03-06 11:21:54 +0600

    Merged temp into mainfinally

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,7 @@
     <x-slot:heading>
         Home Page
     </x-slot:heading>
-<<<<<<< HEAD
-    <h3>Hello from the home page</h3>
-=======
+    
     <h3>goodmoring from the home page</h3>
->>>>>>> temp
+
 </x-layout>
